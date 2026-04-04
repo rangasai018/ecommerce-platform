dd4b9772d11315752514864c083c3cd107acaccd
Ranga Sai <chiraboinarangasai@gmail.com>
2026-04-04 11:34:35 +0530

    MySQL integration, User entity, Register & Login API

---
+++ backend/src/main/java/com/ecommerce/backend/controller/AuthController.java
@@ -0,0 +1,19 @@
+package com.ecommerce.backend.controller;
+
+import com.ecommerce.backend.model.User;
+import com.ecommerce.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+}
---
+++ backend/src/main/java/com/ecommerce/backend/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.ecommerce.backend.repository;
+
+import com.ecommerce.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
