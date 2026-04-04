ddc3b404db0ec3472e97bd219cb71d6a7a229725
Ranga Sai <chiraboinarangasai@gmail.com>
2026-04-04 10:31:17 +0530

    Backend setup and first API completed

---
+++ backend/src/main/java/com/ecommerce/backend/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.ecommerce.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "E-commerce Backend Running 🚀";
+    }
+}
