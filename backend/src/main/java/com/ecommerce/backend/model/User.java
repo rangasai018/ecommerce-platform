21adddc7762f49c96c9aca8f71f747d63108ec44
Ranga Sai <chiraboinarangasai@gmail.com>
2026-07-02 13:49:54 +0530

    Switch backend from MySQL to H2 database

--- backend/src/main/java/com/ecommerce/backend/model/User.java
+++ backend/src/main/java/com/ecommerce/backend/model/User.java
@@ -3,6 +3,7 @@ package com.ecommerce.backend.model;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
@@ -13,15 +14,31 @@ public class User {
     private String email;
     private String password;
 
-    // getters & setters
-    public Long getId() { return id; }
+    public Long getId() {
+        return id;
+    }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public String getName() {
+        return name;
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
